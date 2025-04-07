c211b78ba9908db2cb14eb4eb80e89f0786fcd7f
Teddy Taussig <tttaussig@gmail.com>
2025-04-07 12:40:41 -0400

    Fix style issues from rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   get "home/contactus"
   get "home/help"
   resources :microposts
-  
+
   root "users#index"
-  
+
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
